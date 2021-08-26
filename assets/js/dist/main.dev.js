ee6e0bf90c1814cf048ff930d0975f32fd48b0e7
Your Name <timaoz900@gmail.com>
2021-08-26 17:08:11 +0300

    add files

---
+++ assets/js/dist/main.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+wow = new WOW({
+  boxClass: 'wow',
+  animateClass: 'animate__animated',
+  offset: 20,
+  mobile: true,
+  live: true
+});
+wow.init();
---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+wow = new WOW(
+    {
+        boxClass: 'wow',    
+        animateClass: 'animate__animated', 
+        offset: 20,         
+        mobile: true,       
+        live: true        
+    }
+)
+wow.init();
